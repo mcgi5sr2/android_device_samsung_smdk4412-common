cd6024d6c9d401e15af87f92156f1f405254a3a2
Dimitri <frager1911@gmail.com>
2015-03-02 01:49:34 +0100

    smdk4412-common: Enable USB-OTG
    Change-Id: Ie680c5eeb59d23cb3637cc02bf71be7d4802945b

--- common.mk
+++ common.mk
@@ -191,6 +191,10 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     media.stagefright.use-awesome=true
 
+# USB-OTG
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.sys.isUsbOtgEnabled=true
+
 # Include exynos4 platform specific parts
 TARGET_HAL_PATH := hardware/samsung/exynos4/hal
 TARGET_OMX_PATH := hardware/samsung/exynos/multimedia/openmax
